f7dc0025af2896465992d21adc1a0a46ea97681b
Mahfuz <md.mahfuzur@brainstation-23.com>
2024-12-31 01:59:13 +0600

    Coffe-shop-booth: code organization using function

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 const (
     CappuccinoPrice = 150
     AmericanoPrice = 250
-    ExpressoPrice = 200
+    EspressoPrice = 200
 )
 
 type Cart struct{
@@ -26,6 +26,69 @@ type Coffee struct{
     price float32;
 }
 
+func displayMenu() {
+    fmt.Println("--------- Self Service Coffee Shop ------------")
+
+    fmt.Println("1. Cappuccino")
+    fmt.Println("2. Americano")
+    fmt.Println("3. Espresso")
+    fmt.Println("4. View Items")
+    fmt.Println("5. Confirm Purchase")
+    fmt.Println("6. Exit")
+}
+
+func getChoice(reader *bufio.Reader) (int, error) {
+    input, _ := reader.ReadString('\n')
+    input = strings.TrimSpace(input)
+
+    return strconv.Atoi(input)
+}
+
+func addItemToCart(choice int, quantity int, coffees map[int]Coffee, cart map[int]Cart) {
+    item, exists := cart[choice]
+    if exists {
+        item.quantity += quantity
+        item.amount = float32(item.quantity) * item.price
+        cart[choice] = item
+    } else {
+        cart[choice] = Cart{
+            name: coffees[choice].name,
+            quantity: quantity,
+            price: coffees[choice].price,
+            amount: coffees[choice].price * float32(quantity),
+        }
+    }
+}
+
+func viewCart(cart map[int]Cart) {
+    if len(cart) == 0 {
+        fmt.Println("Empty bucket. Please select an item first.")
+        return
+    }
+    
+    var totalAmount float32 = 0.0
+    fmt.Printf("%-15s %-15s %-15s %-15s\n", "Item Name", "Price", "Quantity", "Amount")
+    for _, item := range cart {
+        fmt.Printf("%-15s %-15.2f %-15d %-15.2f\n", item.name, item.price, item.quantity, item.amount)
+        totalAmount += item.amount
+    }
+    fmt.Println("---------------------------------------------------------------")
+    fmt.Printf("%-47s %.2f\n", "Total Amount", totalAmount)
+}
+
+func confirmPurchase(cart map[int]Cart) {
+    viewCart(cart)
+
+    var approval string
+    fmt.Print("Type 'yes' for payment: ")
+    fmt.Scanln(&approval)
+
+    if approval == "yes" {
+        fmt.Println("Thanks for shopping with us!")
+        return
+    }
+}
+
 func main() {
 
     coffees := map[int]Coffee {
@@ -38,8 +101,8 @@ func main() {
             price: AmericanoPrice,
         },
         3: {
-            name: "Expresso",
-            price: ExpressoPrice,
+            name: "Espresso",
+            price: EspressoPrice,
         },
     }
 
@@ -47,20 +110,9 @@ func main() {
     reader := bufio.NewReader(os.Stdin)
 
     for {
-        fmt.Println("--------- Self Service Coffee Shop ------------")
-
-        fmt.Println("1. Cappuccino")
-        fmt.Println("2. Americano")
-        fmt.Println("3. Expresso")
-        fmt.Println("4. View Items")
-        fmt.Println("5. Confirm Purchase")
-        fmt.Println("6. Exit")
-
+        displayMenu()
         fmt.Print("Choose an option: ")
-        input, _ := reader.ReadString('\n')
-        input = strings.TrimSpace(input)
-
-        choice, error := strconv.Atoi(input)
+        choice, error := getChoice(reader)
         if error != nil {
             fmt.Println("Invalid input. Please enter a number between 1 to 6")
             continue
@@ -69,64 +121,17 @@ func main() {
         switch choice {
         case 1, 2, 3:
             fmt.Print("Enter quantity: ")
-            quantityInput, _ := reader.ReadString('\n')
-            quantityInput = strings.TrimSpace(quantityInput)
-            quantity, err := strconv.Atoi(quantityInput)
+            quantity, err := getChoice(reader)
             if err != nil || quantity <= 0 {
                 fmt.Println("Invalid quantity. Please enter a valid quantity")
                 continue
             }
 
-            item, exists := selectedItems[choice]
-            if exists {
-                item.quantity += quantity
-                item.amount = float32(item.quantity) * item.price
-                selectedItems[choice] = item
-            } else {
-                selectedItems[choice] = Cart{
-                    name: coffees[choice].name,
-                    quantity: quantity,
-                    price: coffees[choice].price,
-                    amount: coffees[choice].price * float32(quantity),
-                }
-            }
+            addItemToCart(choice, quantity, coffees, selectedItems)
         case 4:
-            if len(selectedItems) == 0 {
-                fmt.Println("Empty bucket. Please select an item first.")
-                continue
-            }
-            
-            var totalAmount float32 = 0.0
-            fmt.Printf("%-15s %-15s %-15s %-15s\n", "Item Name", "Price", "Quantity", "Amount")
-            for _, item := range selectedItems {
-                fmt.Printf("%-15s %-15.2f %-15d %-15.2f\n", item.name, item.price, item.quantity, item.amount)
-                totalAmount += item.amount
-            }
-            fmt.Println("---------------------------------------------------------------")
-            fmt.Printf("%-47s %.2f\n", "Total Amount", totalAmount)
+            viewCart(selectedItems)
         case 5:
-            if len(selectedItems) == 0 {
-                fmt.Println("Empty bucket. Please select an item first.")
-                continue
-            }
-
-            var totalAmount float32 = 0.0
-            fmt.Printf("%-15s %-15s %-15s %-15s\n", "Item Name", "Price", "Quantity", "Amount")
-            for _, item := range selectedItems {
-                fmt.Printf("%-15s %-15.2f %-15d %-15.2f\n", item.name, item.price, item.quantity, item.amount)
-                totalAmount += item.amount
-            }
-            fmt.Println("---------------------------------------------------------------")
-            fmt.Printf("%-47s %.2f\n", "Total Amount", totalAmount)
-
-            var approval string
-            fmt.Print("Type 'yes' for payment: ")
-            fmt.Scanln(&approval)
-
-            if approval == "yes" {
-                fmt.Println("Thanks for shopping with us!")
-                return
-            }
+            confirmPurchase(selectedItems)
         case 6:
             fmt.Println("Existing from the coffee purchasing booth. Goodbye!")
             return
